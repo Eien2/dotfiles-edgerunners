1c01b1cb198ff71f95cc5984ed04adfe8353e35e
Eien <oleksiaksebastian32@gmail.com>
2024-08-23 17:23:20 +0200

    deleted accident copy of nvim in nvim catalog

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,9 +4,9 @@
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', 'C-f>', builtin.find_files, {})
-      vim.keymap.set('n', 'C-g', builtin.live_grep, {})
-      vim.keymap.set('n', 'C-c', builtin.git_commits, {})
+      vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+      vim.keymap.set('n', '<C-c>', builtin.git_commits, {})
     end
     },
     {
